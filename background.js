89f17a3235992e486d0ebeeceb4887586a198c70
Divij Chopra <dichopra@microsoft.com>
2022-07-18 02:31:54 +0530

    add intial code for DownloadOragnizer extension

---
+++ background.js
@@ -0,0 +1,41 @@
+const storageCache = {};
+const initStorageCache = getAllStorageSyncData().then(items => {
+  Object.assign(storageCache, items);
+});
+
+let askPathForFileType = (fileExtenstion) => {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      fileExtenstion: fileExtenstion
+    }, 
+    function(response) {
+      let storageApiRequest = {};
+      storageApiRequest["suggestedPath->" + fileExtenstion] = response.path;
+      storageCache[`suggestedPath->` + fileExtenstion] = response.path;
+      chrome.storage.sync.set(storageApiRequest);
+    });
+  });
+}
+
+chrome.downloads.onDeterminingFilename.addListener(
+  (downloadItem, suggest) => {
+    let filePath = downloadItem.filename;
+    let fileExtenstion = filePath.split(".").at(-1).trim().toLowerCase();
+    let fileName = filePath.split("\\").at(-1).trim();
+    let key = "suggestedPath->" + fileExtenstion;
+
+    if (storageCache[key] == undefined)
+    {
+      askPathForFileType(fileExtenstion);
+    }
+    let filenameSuggestion = {
+      conflictAction: "uniquify",
+      filename: storageCache[key] + "\\" + fileName,
+    }
+
+    console.log(downloadItem.mime);
+
+    suggest(filenameSuggestion);
+  }
+)
+
